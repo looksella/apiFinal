7b031d5fa57e843dc3df5d583fb1d4bba6b62a88
Alejo <alejo7d1@gmail.com>
2025-12-03 00:22:41 -0600

    Fixed: Multiple errors during the merge

--- routes/api.php
+++ routes/api.php
@@ -8,27 +8,23 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login',    [AuthController::class, 'login']);
 Route::post('/refresh',  [AuthController::class, 'refresh']);
 
-//Rutas protegidas
-Route::middleware('auth:sanctum')->group(function () {
-    Route::get('/me', [AuthController::class, 'me']); //clase protegida de ejemplo, retorna info de el usuario
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-
 // Endpoints publicos
-Route::get('products', [ProductController::class, 'index']);
-Route::get('products/best', [ProductController::class, 'best']);
-Route::get('products/{product}', [ProductController::class, 'show']);
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/best', [ProductController::class, 'best']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
 
 
 // Endpoints protegidos requieren usuario autenticado - Sanctum o similar
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('products', [ProductController::class, 'store']);
-    Route::put('products/{product}', [ProductController::class, 'update']);
-    Route::patch('products/{product}', [ProductController::class, 'update']);
-    Route::delete('products/{product}', [ProductController::class, 'destroy']);
+    Route::get('/me', [AuthController::class, 'me']); //clase protegida de ejemplo, retorna info de el usuario
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{product}', [ProductController::class, 'update']);
+    Route::patch('/products/{product}', [ProductController::class, 'update']);
+    Route::delete('/products/{product}', [ProductController::class, 'destroy']);
 
-    Route::post('products/{product}/reviews', [ProductReviewController::class, 'store']);
-    Route::put('reviews/{review}', [ProductReviewController::class, 'update']);
-    Route::patch('reviews/{review}', [ProductReviewController::class, 'update']);
-    Route::delete('reviews/{review}', [ProductReviewController::class, 'destroy']);
+    Route::post('/products/{product}/reviews', [ProductReviewController::class, 'store']);
+    Route::put('/reviews/{review}', [ProductReviewController::class, 'update']);
+    Route::patch('/reviews/{review}', [ProductReviewController::class, 'update']);
+    Route::delete('/reviews/{review}', [ProductReviewController::class, 'destroy']);
 });
